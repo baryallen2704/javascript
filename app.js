535ff75dce3a4f7208541e1dfba69bd677593abb
unknown <joelsantiagocubillos@gmail.com>
2019-10-20 17:53:17 +0200

    fits commit

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("hl soy santiago");
+var distancia =200; // esto define una variable que se llama distancia todas las variables se definen con var
+if (distancia >=100) // esto es una condicional en donde pregunta si distancia es mayor o igual a 100
+console.log(distancia);
+
+var minombre="hola soy santiago"+" cubillos quintero this is my first page";
+console.log(minombre);
+
+var santiago =50;
+var joel =20;
+
+var resultado =santiago + joel;
+console.log(resultado);
+
+var resultado2 =resultado*120 ;
+console.log (resultado2);
+
+//como realizar el ciclo for//
+for(var cubillos=10;cubillos>0;cubillos=cubillos-1)
+{console.log(cubillos)};
+
+
+
+//hacer un ciclo for que imprima los numeros del 10 al 0 javescrpt.//
